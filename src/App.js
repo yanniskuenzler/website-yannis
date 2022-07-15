767c5dfc7d944813da5179295d4906300723a96b
Yannis Künzler <y.kuenzler@icloud.com>
2022-07-15 09:39:54 +0200

    added routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
+import { Routes, Route, Link } from "react-router-dom";
+
+import Home from "./components/Home";
+import About from "./components/About";
 
 function App() {
   return (
-      <div>
-
-      </div>
+      <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+      </Routes>
   );
 }
 
