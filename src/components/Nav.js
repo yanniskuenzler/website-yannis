ef3b748a045c6d65aa4799f1fbeb5fe321d696e8
Yannis Künzler <y.kuenzler@icloud.com>
2022-07-21 23:53:28 +0200

    changed background color of nav to nav element instead in the ul

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,8 +3,8 @@ import BasicNavLinks from "./BasicNavLinks";
 function Nav() {
     return (
         <div>
-            <nav>
-                <ul className="flex space-x-20 justify-center bg-neutral-900">
+            <nav className="py-1 bg-neutral-900">
+                <ul className="flex space-x-20 justify-center">
                     <BasicNavLinks siteName="Home" />
                     <BasicNavLinks siteName="About" />
                     <BasicNavLinks siteName="Apps" />
