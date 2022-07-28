2cd685cc36d03b37109f3dff235d503d6e511670
Yannis Künzler <y.kuenzler@icloud.com>
2022-07-28 21:54:47 +0200

    changed flex in todo app

--- src/components/Apps/TodoList/TodoItem.js
+++ src/components/Apps/TodoList/TodoItem.js
@@ -1,11 +1,28 @@
 function TodoItem(props) {
     return (
-        <div className="flex gap-2 border p-2 mt-1 bg-gray-400">
-            <p className="flex-col basis-3/4">{props.item.content}</p>
-            <div className="flex flex-col basis-1/4">
-                <button onClick={(id) => props.handleDelete(props.item.id)} className="bg-red-600 p-1 m-1">Löschen</button>
-                <button onClick={(id) => props.handleDelete(props.item.id)} className="bg-green-600 p-1 m-1">Erledigt</button>
+        <div className="flex gap-3 border p-2 mt-1 bg-gray-400">
+            <div className="flex-col">
+                <button
+                    onClick={(id) => props.handleDelete(props.item.id)}
+                    className="bg-green-600 hover:bg-green-500 p-1 m-1 rounded"
+                >
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="h-6 w-6"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                        stroke-width="2"
+                    >
+                        <path
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
+                        />
+                    </svg>
+                </button>
             </div>
+            <p className="flex-col">{props.item.content}</p>
         </div>
     )
 }
